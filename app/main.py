67dc20bf4c36d2e8b49c7b9a3c2346cc4ed68544
Bixente Grandjean <bixente.grandjean@gmail.com>
2025-10-24 21:20:09 +0200

    [*] CORS

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI, Depends
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.schemas.database_schemas import (
     AgendaList,
@@ -25,6 +26,16 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
